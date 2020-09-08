9ba604f54b4df5658f8aaf53394d4141dcd22e0d
Iyosayi <Iyosa14@gmail.com>
2020-09-08 22:45:27 +0100

    fix

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,51 +1,64 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './login.css'
+import React from "react";
+import { Link } from "react-router-dom";
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./login.css";
 
-export default function login(){
-        const bodyStyle = document.querySelector("body").style;
-        bodyStyle.backgroundImage = "linear-gradient(to bottom, #F6F6F2, #C2EDCE)";
-        bodyStyle.height = "100%"
-        bodyStyle.margin = "0"
-        bodyStyle.backgroundRepeat = "no-repeat"
-        bodyStyle.backgroundAttachment = "fixed"
-        bodyStyle.fontFamily = "Rubik, sans-serif"; 
+export default function login() {
+  const bodyStyle = document.querySelector("body").style;
+  bodyStyle.backgroundImage = "linear-gradient(to bottom, #F6F6F2, #C2EDCE)";
+  bodyStyle.height = "100%";
+  bodyStyle.margin = "0";
+  bodyStyle.backgroundRepeat = "no-repeat";
+  bodyStyle.backgroundAttachment = "fixed";
+  bodyStyle.fontFamily = "Rubik, sans-serif";
 
-        return(
-            <div className="container-fluid mt-5">
-                <div className="justify-content-center">
-                    <div id="login-container" className="mx-auto">
-                        <form>
-                            <div id="login-form-inputs" className="form-group">
-                                <p className="text-center" style={{fontSize: "1.5rem", color: "#388087"}}>LOGIN</p>
-                                <input 
-                                type="email"
-                                className="form-control"
-                                id="email-address"
-                                placeholder="Email address"></input>
-                                
-                                <input 
-                                type="password"
-                                className="form-control"
-                                id="password"
-                                placeholder="Password"></input>
-                            </div>
-                            <button type="submit" className="btn">Submit</button>
-                        </form>
-                    </div>
-                    <div className="mx-auto text-light py-3" id="login-cta">
-                        <p className="text-center text-dark">
-                            New to MoneyGuard? 
-                            <p>
-                                <Link to={"/signup"} className="text-center text-dark"><b>Sign up</b></Link>
-                                <br></br>
-                                <Link to={"#"} className="text-center text-dark"><b>Forgot your password?</b></Link>
-                            </p>
-                        </p>
-                    </div>
-                </div>
+  return (
+    <div className="container-fluid mt-5">
+      <div className="justify-content-center">
+        <div id="login-container" className="mx-auto">
+          <form>
+            <div id="login-form-inputs" className="form-group">
+              <p
+                className="text-center"
+                style={{ fontSize: "1.5rem", color: "#388087" }}
+              >
+                LOGIN
+              </p>
+              <input
+                type="email"
+                className="form-control"
+                id="email-address"
+                placeholder="Email address"
+              ></input>
+
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+              ></input>
             </div>
-        )
+            <button type="submit" className="btn">
+              Submit
+            </button>
+          </form>
+        </div>
+        <div className="mx-auto text-light py-3" id="login-cta">
+          <p className="text-center text-dark">
+            New to MoneyGuard?
+            <p>
+              <Link to={"/signup"} className="text-center text-dark">
+                <b>Sign up</b>
+              </Link>
+              <br></br>
+              <Link to={"#"} className="text-center text-dark">
+                <b>Forgot your password?</b>
+              </Link>
+            </p>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Signup/signup.jsx
+++ src/components/Signup/signup.jsx
@@ -1,76 +1,91 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './signup.css';
+import React from "react";
+import { Link } from "react-router-dom";
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./signup.css";
 
-export default function Signup (){
-        const bodyStyle = document.querySelector("body").style;
-        bodyStyle.backgroundImage = "linear-gradient(to bottom, #F6F6F2, #C2EDCE)";
-        bodyStyle.backgroundRepeat = "no-repeat";
-        bodyStyle.overflow = "hidden"
-        bodyStyle.height = "100%"
-        bodyStyle.fontFamily = "Rubik, sans-serif"; 
-        
-        return(
-            <div id="container-fluid" className="container-fluid py-4">
-                <div id="reg-form-container">
-                    <div id="info-column">
-                        <h3 className="text-light">INFORMATION</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur ipsum dolor sit amet consectetur ipsum dolor sit amet consectetur ipsum dolor sit amet consectetur ipsum dolor sit amet consectetur</p>
-                        <Link to="/login">
-                            <button type="button" className="btn btn-light">Have an account?</button>
-                        </Link>
-                    </div>
-                    <div id="reg-column">
-                        <h3 style={{color: "#388087"}}>REGISTRATION</h3>
-                        <form>
-                            <div id="form-row1-label">
-                                <div id="firstNameLabel">
-                                    <label htmlFor="firstName">First Name</label>
-                                </div>
-                                <div id="lastNameLabel">
-                                    <label id="lastName">Last Name</label>
-                                </div>
-                            </div>
-                            <div id="form-row1-input">
-                                <div id="firstNameInput">
-                                    <input type="text" name="firstName" id="firstName"/>
-                                </div>
-                                <div id="lastNameInput">
-                                    <input type="text" name="lastName" id="lastName"/>
-                                </div>
-                            </div>
-                            <div id="form-row2-label">
-                                <label htmlFor="email">Your Email</label>
-                            </div>
-                            <div id="form-row2-input">
-                                <input type="email" name="email" id="email"/>
-                            </div>
-                            <div id="form-row3-label">
-                                <label htmlFor="phonenumber">Phone Number</label>
-                            </div>
-                            <div id="form-row3-input">
-                                <input type="text" name="phonenumber" id="phonenumber" />
-                            </div>
-                            <div id="form-row4-label">
-                                <label htmlFor="username">User Name</label>
-                            </div>
-                            <div id="form-row4-input">
-                                <input type="text" name="username" id="username"/>
-                            </div>
-                            <div id="form-row5-label">
-                                <label htmlFor="password">Password</label>
-                            </div>
-                            <div id="form-row5-input">
-                                <input type="password" name="password" id="password"/>
-                            </div>
-                            <input type="checkbox" name="termsAndConditions" id="termsAndConditions" style={{marginRight: "5px"}} required/>I agree to the <Link to="#">Terms and Conditions</Link>
-                            <br/>
-                            <button id="btn-register" type="submit">Register</button>
-                        </form>
-                    </div>
-                </div>
+export default function Signup() {
+  const bodyStyle = document.querySelector("body").style;
+  bodyStyle.backgroundImage = "linear-gradient(to bottom, #F6F6F2, #C2EDCE)";
+  bodyStyle.backgroundRepeat = "no-repeat";
+  bodyStyle.overflow = "hidden";
+  bodyStyle.height = "100%";
+  bodyStyle.fontFamily = "Rubik, sans-serif";
+
+  return (
+    <div id="container-fluid" className="container-fluid py-4">
+      <div id="reg-form-container">
+        <div id="info-column">
+          <h3 className="text-light">INFORMATION</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur ipsum dolor sit amet
+            consectetur ipsum dolor sit amet consectetur ipsum dolor sit amet
+            consectetur ipsum dolor sit amet consectetur
+          </p>
+          <Link to="/login">
+            <button type="button" className="btn btn-light">
+              Have an account?
+            </button>
+          </Link>
+        </div>
+        <div id="reg-column">
+          <h3 style={{ color: "#388087" }}>REGISTRATION</h3>
+          <form>
+            <div id="form-row1-label">
+              <div id="firstNameLabel">
+                <label htmlFor="firstName">First Name</label>
+              </div>
+              <div id="lastNameLabel">
+                <label id="lastName">Last Name</label>
+              </div>
             </div>
-        )
+            <div id="form-row1-input">
+              <div id="firstNameInput">
+                <input type="text" name="firstName" id="firstName" />
+              </div>
+              <div id="lastNameInput">
+                <input type="text" name="lastName" id="lastName" />
+              </div>
+            </div>
+            <div id="form-row2-label">
+              <label htmlFor="email">Your Email</label>
+            </div>
+            <div id="form-row2-input">
+              <input type="email" name="email" id="email" />
+            </div>
+            <div id="form-row3-label">
+              <label htmlFor="phonenumber">Phone Number</label>
+            </div>
+            <div id="form-row3-input">
+              <input type="text" name="phonenumber" id="phonenumber" />
+            </div>
+            <div id="form-row4-label">
+              <label htmlFor="username">User Name</label>
+            </div>
+            <div id="form-row4-input">
+              <input type="text" name="username" id="username" />
+            </div>
+            <div id="form-row5-label">
+              <label htmlFor="password">Password</label>
+            </div>
+            <div id="form-row5-input">
+              <input type="password" name="password" id="password" />
+            </div>
+            <input
+              type="checkbox"
+              name="termsAndConditions"
+              id="termsAndConditions"
+              style={{ marginRight: "5px" }}
+              required
+            />
+            I agree to the <Link to="#">Terms and Conditions</Link>
+            <br />
+            <button id="btn-register" type="submit">
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
