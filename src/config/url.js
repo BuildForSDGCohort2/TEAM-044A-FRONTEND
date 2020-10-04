7c9ba310b32163843635969e97445131298ba8b9
Iyosayi <Iyosa14@gmail.com>
2020-10-04 18:12:35 +0100

    fixed endpoints

--- src/config/url.js
+++ src/config/url.js
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export default process.env.HOME_URL === "production"
-  ? "https://moneyguard.herokuapp.com/api/v1"
-  : "http://localhost:4000/api/v1";
+const API_URL =
+  process.env.HOME_URL === "production"
+    ? "https://moneyguard.herokuapp.com/api/v1"
+    : "http://localhost:4000/api/v1";
+
+export default API_URL;
